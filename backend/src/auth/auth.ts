45ce12a1b411c387174337fe1d8d4ad8038c6ec2
Katanon Krengsomrong <nn.katanon@gmail.com>
2026-06-13 01:34:40 +0700

    feat:Add better-auth & setup in project

---
+++ backend/src/auth/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { admin, openAPI } from "better-auth/plugins"
+import { Pool } from "pg"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client"
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    openAPI(),
+    // admin(),
+  ],
+})
